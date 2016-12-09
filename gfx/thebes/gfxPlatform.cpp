05ddc74139b429892b4711d90eda767f336617aa
Raine Makelainen <raine.makelainen@jolla.com>
2016-12-09 15:01:12 +0200

    *mb2-prepare* Fix gfxPlatform::AsyncPanZoomEnabled for embedlite. JB#50863
    sailfishos-esr52 sha1: 017d7e40194c059a2bc8b24ff3aef25f96faf6a3
    sailfishos-esr60 sha1: 409d3a8e50829f9f4c35a4707fae53a45515ce5b
    
    Signed-off-by: Raine Makelainen <raine.makelainen@jolla.com>
    Mb2-scrape: filename=0033-Fix-gfxPlatform-AsyncPanZoomEnabled-for-embedlite.-J.patch

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -3820,7 +3820,7 @@ bool gfxPlatform::UseDesktopZoomingScrollbars() {
 
 /*static*/
 bool gfxPlatform::AsyncPanZoomEnabled() {
-#if !defined(MOZ_WIDGET_ANDROID) && !defined(MOZ_WIDGET_UIKIT)
+#if !defined(MOZ_WIDGET_ANDROID) && !defined(MOZ_WIDGET_UIKIT) && !defined(MOZ_WIDGET_QT)
   // For XUL applications (everything but Firefox on Android)
   // we only want to use APZ when E10S is enabled. If
   // we ever get input events off the main thread we can consider relaxing
