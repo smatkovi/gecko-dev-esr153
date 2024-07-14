79493a0eb27ba22e96e4e6aa869ae61ff9254046
David Llewellyn-Jones <david@flypig.co.uk>
2024-07-14 23:03:42 +0100

    *mb2-prepare* Drop AudioPlayback messages if no embedder element is defined
    The AudioPlayback Window Actor passes "audio-playback" messages on to
    the embedder element by calling it directly. If the embedder element is
    null this will result in a "browser is null" error being output. This
    doesn't do any real harm, but is unnecessary and could cause confusion.
    This change adds a null check so that an attempt to call methods off the
    browser only happen if it's defined.
    
    On Sailfish OS the embedder element is always null, so there's no need
    (or way) to pass these calls on anyway.
    
    Mb2-scrape: filename=0045-Drop-AudioPlayback-messages-if-no-embedder-element-i.patch

--- toolkit/actors/AudioPlaybackParent.sys.mjs
+++ toolkit/actors/AudioPlaybackParent.sys.mjs
@@ -10,6 +10,9 @@ export class AudioPlaybackParent extends JSWindowActorParent {
   }
   receiveMessage(aMessage) {
     const browser = this.browsingContext.top.embedderElement;
+    if (!browser) {
+      return;
+    }
     switch (aMessage.name) {
       case "AudioPlayback:Start":
         this._hasAudioPlayback = true;
