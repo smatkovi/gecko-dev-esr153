74bcc0c7ce1f963419efb0d002000d7d8f891f18
sm <sebstianmatkovich@gmail.com>
2026-08-20 00:17:06 +0200

    embedlite-140: Ladefront durchbrochen - single-process erzwingen
    Gecko 140 versucht fuer jeden Dokument-Load einen Prozess-Switch
    (ChangeRemoteness), der im Single-Process-EmbedLite ins Leere greift
    (8000ffff), und blockt danach file:// im Parent (805e0006).
    
    - DocumentLoadListener: kein Prozess-Switch ohne UseRemoteTabs
    - nsAppRunner: BrowserTabsRemoteAutostart() unter MOZ_EMBEDLITE false
    - WebRenderBridgeParent: dlBytes+TxnURL-Diagnose
    
    Erste echte Seiten-DisplayLists der Portierung (dlBytes 1113-5538,
    13x rich.html ohne Env-Kruecken).

--- gfx/layers/wr/WebRenderBridgeParent.cpp
+++ gfx/layers/wr/WebRenderBridgeParent.cpp
@@ -1272,9 +1272,11 @@ mozilla::ipc::IPCResult WebRenderBridgeParent::RecvSetDisplayList(
   }
 
   LOG("WebRenderBridgeParent::RecvSetDisplayList() PipelineId %" PRIx64
-      " Id %" PRIx64 " root %d",
+      " Id %" PRIx64 " root %d dlBytes %zu url %s",
       wr::AsUint64(mPipelineId), wr::AsUint64(mApi->GetId()),
-      IsRootWebRenderBridgeParent());
+      IsRootWebRenderBridgeParent(),
+      aDisplayList.mDLItems ? size_t(aDisplayList.mDLItems->mLen) : size_t(0),
+      PromiseFlatCString(aTxnURL).get());
 
   if (!IsRootWebRenderBridgeParent()) {
     CrashReporter::RecordAnnotationNSCString(CrashReporter::Annotation::URL,
